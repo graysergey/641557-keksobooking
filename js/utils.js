9f68ce98ab7ef44038f8fe4b103844629185f177
GraySergey <xgortx@gmail.com>
2018-12-11 21:35:38 +0200

    Выносит из объекта функции, записывает в объект utiils только название функций

--- js/utils.js
+++ js/utils.js
@@ -5,33 +5,39 @@
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
 
-  var utils = {
-    // Перемешивает массив
-    shuffleArray: function (array) {
-      for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-      return array;
-    },
-
-    getRandomNumber: function (from, to) {
-      return Math.floor(Math.random() * (to - from + 1) + from);
-    },
-
-    getRandomLengthArray: function (array) {
-      return array.slice(utils.getRandomNumber(1, array.length - 1));
-    },
-
-    isEnterEvent: function (evt) {
-      return evt.keyCode === ENTER_KEYCODE;
-    },
-
-    isEscapeEvt: function (evt) {
-      return evt.keyCode === ESC_KEYCODE;
+  // Перемешивает массив
+  var shuffleArray = function (array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
+    return array;
+  };
+
+  var getRandomNumber = function (from, to) {
+    return Math.floor(Math.random() * (to - from + 1) + from);
+  };
+
+  var getRandomLengthArray = function (array) {
+    return array.slice(utils.getRandomNumber(1, array.length - 1));
+  };
+
+  var isEnterEvent = function (evt) {
+    return evt.keyCode === ENTER_KEYCODE;
+  };
+
+  var isEscapeEvt = function (evt) {
+    return evt.keyCode === ESC_KEYCODE;
+  };
+
+  var utils = {
+    shuffleArray: shuffleArray,
+    getRandomNumber: getRandomNumber,
+    getRandomLengthArray: getRandomLengthArray,
+    isEnterEvent: isEnterEvent,
+    isEscapeEvt: isEscapeEvt
   };
 
   window.utils = utils;
