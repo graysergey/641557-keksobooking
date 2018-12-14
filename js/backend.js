5e8dfa81b23f32fe4fe15fc0a0b47444ed945947
GraySergey <xgortx@gmail.com>
2018-12-14 16:26:45 +0200

    Создает пустые функции и объекты для работы с сервером, в модуле backend

--- js/backend.js
+++ js/backend.js
@@ -2,6 +2,27 @@
 
 (function () {
 
+  var URL = {
+    load: 'https://js.dump.academy/keksobooking/data',
+    upload: 'https://js.dump.academy/keksobooking'
+  };
 
+  var load = function () {
+
+  };
+
+  var upload = function () {
+
+  };
+
+  var onError = function () {
+
+  };
+
+  window.backend = {
+    load: load,
+    upload: upload,
+    onError: onError
+  };
 
 })();
