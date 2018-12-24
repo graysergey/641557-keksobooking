1f669c1883b4b911100fd7e27049231a1fcc772f
GraySergey <xgortx@gmail.com>
2018-12-24 22:02:49 +0200

    Добавляет активацию/деактивацию - формы фильтрации меток

---
+++ js/form-filter.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function () {
+
+  var filtersForm = document.querySelector('.map__filters');
+  var selectsFormFilters = filtersForm.querySelectorAll('select');
+  var fieldsetFormFilter = filtersForm.querySelector('fieldset');
+
+  var filtersDeactivate = function () {
+    selectsFormFilters.forEach(function (item) {
+      item.setAttribute('disabled', true);
+    });
+    fieldsetFormFilter.setAttribute('disabled', true);
+  };
+  filtersDeactivate();
+
+  var filtersActivate = function () {
+    selectsFormFilters.forEach(function (item) {
+      item.removeAttribute('disabled');
+    });
+    fieldsetFormFilter.removeAttribute('disabled');
+  };
+
+  window.formFilter = {
+    filtersDeactivate: filtersDeactivate,
+    filtersActivate: filtersActivate
+  };
+
+})();
--- js/move-main-pin.js
+++ js/move-main-pin.js
@@ -23,6 +23,7 @@ mapPinMain.addEventListener('mousedown', function (evt) {
     var isMapActive = document.querySelector('.map--faded');
     if (isMapActive) {
       window.map.activateInterface();
+      window.formFilter.filtersActivate();
     }
 
     var shift = {
